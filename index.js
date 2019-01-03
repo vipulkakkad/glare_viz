b56e514b7984a99f9559d83f97140d176a1def70
Vipul Kakkad <vipul.r.kakkad@gmail.com>
2019-01-03 13:32:28 -0800

    fix filenames

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ var createScene = function (gameParameters) {
 
 /******* End of the create scene function ******/
 var fileIo = new GlareSim.FileIo();
-fileIo.LoadParametersFromFileAsync("InputFiles/singleGear.json", () => {
+fileIo.LoadParametersFromFileAsync("InputFiles/twoGears.json", () => {
     var scene = createScene(fileIo.GameParameters); //Call the createScene function
 
     // Register a render loop to repeatedly render the scene

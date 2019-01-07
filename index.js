fef1f0e47b09f2f6695750606673b8ba3da0acf4
Vipul Kakkad <vipka@microsoft.com>
2019-01-06 20:04:35 -0800

    FreeCamerA

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ var createScene = function (gameParameters) {
     var scene = new BABYLON.Scene(engine);
 
     // Add a camera to the scene and attach it to the canvas
-    var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 5), scene);
-    camera.attachControl(canvas, true);
+    // var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 5), scene);
+    // camera.attachControl(canvas, true);
+    var camera = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(50, 37, -100), scene);
 
     // Add lights to the scene
     var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
